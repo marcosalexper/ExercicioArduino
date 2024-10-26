3ccebee783e59aaa151c082688ab691bc616e94b
Antonio Victor Iaroseski Segala <161530566+antoniovsegala@users.noreply.github.com>
2024-10-25 23:23:03 -0300

    Update ativ9.ino

--- ativ9/ativ9.ino
+++ ativ9/ativ9.ino
@@ -1,18 +1,11 @@
-1. // Luminosidade do fotoresistor
-2. int luminosidade = 0;
-3.
-4. void setup()
-5. {
-6. // Porta 0 (Analógica) em modo de entrada(leitura) para o fotoresistor
-7. pinMode(A0, INPUT);
-8. }
-9.
-10. void loop()
-11. {
-12. // Leitura do fotoresistor
-13. luminosidade = analogRead(A0);
-14. // Saída da variável no monitor serial
-15. Serial.println(luminosidade);
-16. // Atrase um pouco para melhorar o desempenho da simulação
-17. delay(10);
-18. }
+int luminosidade = 0;
+void setup()
+{
+pinMode(A0, INPUT);
+}
+void loop()
+{
+luminosidade = analogRead(A0);
+Serial.println(luminosidade);
+delay(10);
+}
