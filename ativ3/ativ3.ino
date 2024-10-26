819af1b7f110323c9a4abf9dda474fa47ec6d5e0
Antonio Victor Iaroseski Segala <161530566+antoniovsegala@users.noreply.github.com>
2024-10-25 23:08:15 -0300

    Update ativ3.ino

--- ativ3/ativ3.ino
+++ ativ3/ativ3.ino
@@ -1,25 +1,23 @@
 int tempo = 0;
-2.
-3. void setup()
-4. {
-5. // Porta 13 (LED_BUILTIN) em modo de saída (escrita)
-6. pinMode(LED_BUILTIN, OUTPUT);
-7. tempo = 1000;
-8. }
-9.
-10. void loop()
-11. {
-12. delay(tempo); // Aguarda 1000 milésimos de segundo
-13. digitalWrite(LED_BUILTIN, LOW); //Desliga o LED
-14. delay(tempo); // Aguarda 1000 milésimos de segundo
-15. digitalWrite(LED_BUILTIN, HIGH); //Liga o LED
-16. }
+void setup()
+{
+pinMode(LED_BUILTIN, OUTPUT);
+tempo = 1000;
+}
+
+void loop()
+{
+delay(tempo); // Aguarda 1000 milésimos de segundo
+digitalWrite(LED_BUILTIN, LOW); //Desliga o LED
+delay(tempo); // Aguarda 1000 milésimos de segundo
+digitalWrite(LED_BUILTIN, HIGH); //Liga o LED
+}
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
