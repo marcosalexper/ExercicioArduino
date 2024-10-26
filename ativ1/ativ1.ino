cbafef43708e8755fb859f1aa9f58a71e16c9dfa
Antonio Victor Iaroseski Segala <161530566+antoniovsegala@users.noreply.github.com>
2024-10-25 22:53:35 -0300

    Update ativ1.ino

--- ativ1/ativ1.ino
+++ ativ1/ativ1.ino
@@ -1,13 +1,12 @@
 void setup()
-2. {
-3. // Porta 13 (LED_BUILTIN) em modo de saída (escrita)
-4. pinMode(LED_BUILTIN, OUTPUT);
-5. }
-6.
-7. void loop()
-8. {
-9. delay(1000); // Aguarda 1000 milésimos de segundo
-10. digitalWrite(LED_BUILTIN, LOW); // Desliga o LED
-11. delay(1000); // Aguarda 1000 milésimos de segundo
-12. digitalWrite(LED_BUILTIN, HIGH); // Liga o LED
-13. }
+{
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+delay(1000);
+digitalWrite(LED_BUILTIN, LOW); 
+delay(1000); 
+digitalWrite(LED_BUILTIN, HIGH); 
+}
